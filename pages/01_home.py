f388493bb5a31c1e93aead3c8a2af61ee3b6ad40
jameskim707 <kimhaeyong9142@gmail.com>
2025-12-05 23:01:57 +0900

    Update 01_home.py

--- pages/01_home.py
+++ pages/01_home.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
-def run():
-    st.title("🏠 Home")
-    st.write("Lyra Emotional & Investment Guardian에 오신 것을 환영합니다.")
-
+st.title("🏠 Home")
+st.write("Lyra Emotional & Investment Guardian에 오신 것을 환영합니다.")
